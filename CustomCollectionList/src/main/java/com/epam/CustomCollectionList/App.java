cb7e505f8eb2610a865ab89bbbb38e2b3265b1d2
Shamini2000 <alleshamini@gmail.com>
2020-03-05 20:58:32 +0530

    First commit of Custom Collection

---
+++ CustomCollectionList/src/main/java/com/epam/CustomCollectionList/App.java
@@ -0,0 +1,12 @@
+package com.epam.CustomCollectionList;
+
+public class App {
+
+	public static void main( String[] args ) {
+
+    	CustomCollectionList c = new CustomCollectionList();
+    	c.takeInput();
+    	
+    }
+	
+}
---
+++ CustomCollectionList/src/main/java/com/epam/CustomCollectionList/PerformListOperations.java
@@ -0,0 +1,36 @@
+package com.epam.CustomCollectionList;
+
+import java.util.Scanner;
+
+public class PerformListOperations {
+	static Scanner sc = new Scanner(System.in);
+	public static void add(CustomList<Object> list) { 
+		System.out.println("Enter the datatype to be inserted");
+		System.out.println("1.Integer");
+		System.out.println("2.String");
+		System.out.println("3.Double");
+		System.out.println("4.Float");
+		int value = sc.nextInt();
+		switch(value) {
+		case 1:
+			System.out.println("Enter integer value");
+			list.add(sc.nextInt());
+			break;
+		case 2:
+			System.out.println("Enter string value");
+			list.add(sc.next());
+			break;
+		case 3:
+			System.out.println("Enter double value");
+			list.add(sc.nextDouble());
+			break;
+		case 4:
+			System.out.println("Enter float valule");
+			list.add(sc.nextFloat());
+			break;
+		default:
+			System.out.println("Enter valid input");
+		}
+	}
+	
+}
